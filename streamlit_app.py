fa9b210ccf5f6981c9c904b8eb07f053af6c3671
PVSwami15 <Poonamvswami@yahoo.co.uk>
2025-01-23 23:24:48 +0000

    add st.df stament

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,8 @@ if ingredients_list:
 
         import requests
         smoothiefroot_response = requests.get("https://FRUITYVICE.com/api/fruit/watermelon")
-        st.text(smoothiefroot_response.json())
+        #st.text(smoothiefroot_response.json())
+        st.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
 
