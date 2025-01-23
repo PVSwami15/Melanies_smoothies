baf74ea61dcdeecc85ac5a141128e6351ccb1711
PVSwami15 <Poonamvswami@yahoo.co.uk>
2025-01-23 23:45:26 +0000

    line44 indention

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,10 +40,10 @@ if ingredients_list:
      
         time_to_insert = st.button('Submit order')
      
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
+            if time_to_insert:
+                session.sql(my_insert_stmt).collect()
         
-            st.success("Your Smoothie is ordered!", icon="✅")
+                 st.success("Your Smoothie is ordered!", icon="✅")
 
         
        
