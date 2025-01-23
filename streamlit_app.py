cfa03707185c1b6d30e7a4aff468b8f786d2ae5d
PVSwami15 <Poonamvswami@yahoo.co.uk>
2025-01-23 23:57:29 +0000

    comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutritrion information')
         smoothiefroot_response = requests.get("https://FRUITYVICE.com/api/fruit/watermelon" + fruit_chosen)
         st.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        st.write(ingredients_string)
+        #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
