30e41e9df3549a7dc28e2455ad4d77c943b43d4d
PVSwami15 <Poonamvswami@yahoo.co.uk>
2025-01-24 21:32:22 +0000

    Update streamlit_app.py
    indention

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
             st.df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
             #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
      
     time_to_insert = st.button('Submit order')
